55e8e5f67ed9f2a5615bb1d2efd09d2827eeeb36
Bolanle Bincom <bolanle.bincom@gmail.com>
2019-01-17 14:58:39 +0100

    edited the server.js file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/splendidhairs'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
@@ -14,3 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/splendidhairs/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
